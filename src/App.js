a01b13a9db69fb4244e866d5d303bfdfc6c4309a
BharathiMagath <115526290+BharathiMagath@users.noreply.github.com>
2024-07-25 15:20:55 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import HomePage from './components/HomePage';
+import './App.css'; // Import global styles
+//import First from './components/First';
+
+const App = () => (
+  <div className="App">
+    <HomePage />
+  </div>
+);
+
+export default App;
